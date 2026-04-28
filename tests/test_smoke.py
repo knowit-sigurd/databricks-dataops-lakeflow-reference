1821ad384dbde83211bbd2691805fa087f7ea750
Sigurd Lislegaard <sigurd.lislegaard@knowit.no>
2026-04-28 19:19:04 +0200

    temp fix for deploy.yml

--- tests/test_smoke.py
+++ tests/test_smoke.py
@@ -1,2 +1,5 @@
-def test_smoke():
-    assert True
+def test_spark_session():
+    from pyspark.sql import SparkSession
+
+    spark = SparkSession.builder.getOrCreate()
+    assert spark is not None
